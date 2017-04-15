530ac02fe48ea2b34de3c019bdf299a5bd2a5565
ehopperdietzel <ehopperdietzel@gmail.com>
2017-04-15 01:38:22 -0300

    First Upload

---
+++ mainwindow.cpp
@@ -0,0 +1,17 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->toolBar->addWidget(ui->widget);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
