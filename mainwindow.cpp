802f4573128ce842aa47d3a45ad78e732ffb1152
ehopperdietzel <ehopperdietzel@gmail.com>
2017-04-16 23:20:15 -0300

    UI Improve

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,12 +9,15 @@
 #include <mpegfile.h>
 #include <attachedpictureframe.h>
 #include <string>
+#include <QMediaPlayer>
+#include <QScrollArea>
 
 using json = nlohmann::json;
 using namespace TagLib;
 
 json localSongsDB;
 QString path = QDir::homePath() + "/Music/Cuarzo Player";
+QMediaPlayer *player = new QMediaPlayer();
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -24,7 +27,6 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->toolBar->addWidget(ui->topBarWidget);
     ui->searchBarInput->setAttribute(Qt::WA_MacShowFocusRect, 0);
     ui->listView->setIconSize(QSize(40,40));
-
     //Read the songs DB
 
     std::ifstream readDB(path.toStdString()+"/localSongsDB.json");
@@ -32,12 +34,24 @@ MainWindow::MainWindow(QWidget *parent) :
 
     displayArtists();
 
+
+    player->setMedia(QUrl::fromLocalFile(path+"/music/Johnny Cash/Sings The Songs That Made Him Famous/Ballad of a Teenage Queen.mp3"));
+    player->setVolume(100);
+    //player->play();
+    connect(player,SIGNAL(positionChanged(qint64)),this,SLOT(setTime(qint64)));
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::setTime(qint64 time){
+    ui->timePosition->setValue(time/1000);
+
+}
+
 
 void MainWindow::displayArtists(){
     ui->listView->clear();
@@ -189,5 +203,143 @@ void MainWindow::on_addMusicButton_clicked()
 
 void MainWindow::on_listView_itemClicked(QListWidgetItem *item)
 {
-    ui->userName->setText(item->text());
+    clearContent();
+
+    //Creates the view inside the scroll area
+    QWidget *view = new QWidget();
+    QBoxLayout *viewLayout = new QBoxLayout(QBoxLayout::TopToBottom,view);
+    viewLayout->setAlignment(Qt::AlignTop);
+
+    //Creates the artist name label
+    QLabel *artistTitle = new QLabel(item->text());
+    artistTitle->setStyleSheet("font-weight:bold;font-size:30px");
+    artistTitle->setSizePolicy(QSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed));
+    artistTitle->setMaximumHeight(40);
+    viewLayout->addWidget(artistTitle);
+
+    for (json::iterator it = localSongsDB["artists"][item->text().toStdString()].begin(); it != localSongsDB["artists"][item->text().toStdString()].end(); ++it) {
+
+        //The album name String
+        QString albumName = QString::fromStdString(it.key());
+
+        //Creates an album widget and layout
+        QWidget *albumWidget = new QWidget();
+        QBoxLayout *albumLayout = new QBoxLayout(QBoxLayout::LeftToRight,albumWidget);
+        albumLayout->setAlignment(Qt::AlignTop);
+        viewLayout->addWidget(albumWidget);
+
+        //Creates the album image and attachs to the album layout
+        QWidget *albumW = new QWidget();
+        QBoxLayout *albumL = new QBoxLayout(QBoxLayout::TopToBottom,albumW);
+        albumL->setAlignment(Qt::AlignTop);
+        QLabel *albumArt = new QLabel();
+        QPixmap albumImage;
+        if(it.value().begin().value()["artwork"] == true){
+            albumImage = QPixmap(path+"/artwork/"+item->text()+"/"+albumName+".png");
+        }
+        else{
+            albumImage = QPixmap(":rec/images/artWork.png");
+        }
+        albumArt->setPixmap(albumImage);
+        albumArt->setStyleSheet("max-height:200px;max-width:200px");
+        albumArt->setScaledContents(true);
+        albumArt->setAlignment(Qt::AlignTop);
+        albumL->addWidget(albumArt);
+        albumLayout->addWidget(albumW);
+
+        //Creates the right side widget and layout and adds to the album layout
+        QWidget *rightContent = new QWidget();
+        QBoxLayout *rightLayout = new QBoxLayout(QBoxLayout::TopToBottom,rightContent);
+        rightLayout->setSpacing(0);
+        rightLayout->setMargin(10);
+        rightLayout->setAlignment(Qt::AlignTop);
+        albumLayout->addWidget(rightContent);
+
+        //Creates the album name label and adds to the right layout
+        QLabel *albumTitle = new QLabel(albumName);
+        albumTitle->setStyleSheet("font-weight:bold;font-size:15px");
+        albumTitle->setSizePolicy(QSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed));
+        albumTitle->setMaximumHeight(18);
+        rightLayout->addWidget(albumTitle);
+
+        //Creates the left and right songs container
+        QWidget *rightLeft = new QWidget();
+        QBoxLayout *rightLeftLayout = new QBoxLayout(QBoxLayout::TopToBottom,rightLeft);
+        rightLayout->addWidget(rightLeft);
+        rightLayout->setAlignment(Qt::AlignRight);
+        rightLeftLayout->setMargin(0);
+
+        //Creates the left songs container
+        QListWidget *leftList = new QListWidget();
+        leftList->setFrameStyle(0);
+        leftList->setFocusPolicy(Qt::NoFocus);
+        leftList->setStyleSheet("max-width:350px");
+        leftList->setSpacing(3);
+        leftList->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+        leftList->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+        rightLeftLayout->addWidget(leftList);
+
+
+        for (json::iterator it2 = it.value().begin(); it2 != it.value().end(); ++it2) {
+
+            //Create the songs
+            QListWidgetItem *song = new QListWidgetItem();
+            QWidget *songItem = new QWidget();
+            QBoxLayout *songItemContainer = new QBoxLayout(QBoxLayout::LeftToRight,songItem);
+
+            songItemContainer->setMargin(0);
+            songItemContainer->setAlignment(Qt::AlignLeft);
+
+            int tr = it2.value()["track"];
+
+            QString tra;
+
+            if(tr < 10){
+                tra = "0"+QString::number(tr);
+            }
+            else{
+                tra = QString::number(tr);
+            }
+
+            QLabel *trackNumber = new QLabel(tra);
+            trackNumber->setStyleSheet("color:#888");
+            songItemContainer->addWidget(trackNumber);
+
+            QString title = QString::fromStdString(it2.value()["title"]);
+            QLabel *trackTitle = new QLabel(title);
+            trackTitle->setStyleSheet("min-width:300px;color:#444;margin-right:20px");
+            songItemContainer->addWidget(trackTitle);
+
+            QToolButton *cloud = new QToolButton();
+            cloud->setIcon(QIcon(":rec/images/download.png"));
+            cloud->setStyleSheet("background:transparent");
+            songItemContainer->addWidget(cloud);
+
+            leftList->addItem(song);
+            leftList->setItemWidget(song,songItem);
+
+        }
+
+    }
+
+    ui->scroll->layout()->addWidget(view);
+    view->show();
+}
+
+void MainWindow::clearContent(){
+    while ( QWidget* w = ui->scroll->findChild<QWidget*>() ){
+        delete w;
+    }
+}
+
+void MainWindow::on_volumeSlider_valueChanged(int value)
+{
+    player->setVolume(value);
+}
+
+
+
+void MainWindow::on_timePosition_sliderMoved(int position)
+{
+     player->setPosition(position*1000);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -14,13 +14,21 @@ class MainWindow : public QMainWindow
 
 public:
     void displayArtists();
+    void clearContent();
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
 private slots:
+    void setTime(qint64);
+
     void on_addMusicButton_clicked();
 
     void on_listView_itemClicked(QListWidgetItem *item);
+
+    void on_volumeSlider_valueChanged(int value);
+
+
+    void on_timePosition_sliderMoved(int position);
 
 private:
     Ui::MainWindow *ui;
