bd67dc96d90715e8ea693addc4d6994a48623167
ehopperdietzel <ehopperdietzel@gmail.com>
2017-05-08 13:03:01 -0300

    Free Storage Bar

--- storagebar.cpp
+++ storagebar.cpp
@@ -7,22 +7,22 @@ extern QString green;
 StorageBar::StorageBar()
 {
     setSizePolicy(QSizePolicy::Fixed,QSizePolicy::Fixed);
-    setFixedSize(100,15);
+    setFixedSize(100,25);
     setObjectName("main");
     setStyleSheet("#main{background:#999;border-radius:4px}");
 
 
     bar->setParent(this);
     bar->setSizePolicy(QSizePolicy::Fixed,QSizePolicy::Fixed);
-    bar->setFixedSize(40,15);
+    bar->setFixedSize(40,25);
     bar->setObjectName("bar");
     bar->setStyleSheet("#bar{background:"+blue+";border-top-left-radius:4px;border-bottom-left-radius:4px}");
     //bar->move(1,1);
 
     text->setParent(this);
     text->setSizePolicy(QSizePolicy::Fixed,QSizePolicy::Fixed);
-    text->setFixedSize(100,15);
-    text->setText("0.1 GB de 15 GB");
+    text->setFixedSize(100,25);
+    text->setText("0.1 GB / 15 GB");
     text->move(10,0);
-    text->setStyleSheet("color:#FFF;text-align:center;font-size:8px");
+    text->setStyleSheet("color:#FFF;text-align:center;font-size:10px");
 }
